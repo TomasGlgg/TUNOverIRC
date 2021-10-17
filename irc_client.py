17c146923a4a64ccb765502cff7bf077758640d4
TomasGlgg <globistomas@gmail.com>
2021-10-17 12:44:53 +0300

    Print error if address already in use

--- irc_client.py
+++ irc_client.py
@@ -9,6 +9,10 @@ class IRC:
         message = event.arguments[0]
         self.privmsg_handler(message)
 
+    def __on_nicknameinuse(self, _, __):
+        print('Error: IP address already in use')
+        exit(1)
+
     def __on_connect(self, _, __):
         print('IRC connected. Nick: {}'.format(self.nick))
         self.client.mode(self.nick, '+Bx')
@@ -18,6 +22,7 @@ class IRC:
         self.client.connect(host, port, nick)
         self.client.add_global_handler('privmsg', self.__on_privmsg)
         self.client.add_global_handler('welcome', self.__on_connect)
+        self.client.add_global_handler('nicknameinuse', self.__on_nicknameinuse)
         self.send = self.client.privmsg
         self.nick = nick
 
